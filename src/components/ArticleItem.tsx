9ca1bc133777090e5f5d5970c31fe8aed41acaaa
Stanislas Manscour <smanscour@groupelexpress.fr>
2018-12-13 19:26:37 +0100

    toggle like btn

--- src/components/ArticleItem.tsx
+++ src/components/ArticleItem.tsx
@@ -3,17 +3,46 @@ import * as React from 'react'
 interface IArticle {
     name: String,
     source: String,
-    price: String
+    price: String,
+    liked: Boolean
 }
 
 interface IProps {
     article: IArticle
 }
 
-class ArticleItem extends React.Component<IProps> {
+
+class ArticleItem extends React.Component<IProps, any> {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            liked: false
+        };
+
+        this.handleLike = this.handleLike.bind(this);
+    }
+
+    handleLike(){
+        this.setState(state => ({
+            liked: !state.liked
+        }))
+    }
+
     render() {
+        let likeButton;
+
+        if (this.state.liked){
+            likeButton = <button onClick={this.handleLike}>Article aimé</button>;
+        } else {
+            likeButton = <button onClick={this.handleLike}>Aimer cet article</button>;
+        }
+
         return(
-            <div>{this.props.article.name} - {this.props.article.price}</div>
+            <div>
+                <p>{this.props.article.name} - {this.props.article.price}</p>
+                { likeButton }
+            </div>
         )
     }
 }
